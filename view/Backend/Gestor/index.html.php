24bbac6c223eb453a0eaef8a99e680e7e5b73e26
esrosales <estefania.rosales.s@gmail.com>
2017-06-03 11:13:54 -0400

    agrege parte del admin

--- view/Backend/Gestor/index.html.php
+++ view/Backend/Gestor/index.html.php
@@ -1,1 +1,127 @@
 <h1>Pantala gestor</h1>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo $tituloPagina; ?></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    </head>
+    <body>
+
+				
+			    <div class="container">
+
+			           <div class="col-md-4">
+
+                <!-- Blog Search Well -->
+                <div class="well">
+                    <h4>RUT PACIENTE</h4>
+                    <div class="input-group">
+                        <input type="text" class="form-control">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">
+                                <span class="glyphicon glyphicon-search"></span>
+                        </button>
+                        </span>
+                    </div>
+                    <!-- /.input-group -->
+                </div>
+
+                <!-- Blog Categories Well -->
+                <div class="well">
+
+                    <h4>DATOS PACIENTES</h4>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <ul class="list-unstyled">
+                                <li><p>NOMBRE</p>
+                                </li>
+                                <li><p>APELLIDO</p>
+                                </li>
+                                <li><p>EDAD</p>
+                                </li>
+                                <li><p>FECHA NACIMIENTO</p>
+                                </li>
+                                <li><p>TELEFONO</p>
+                                </li>
+                                <li><p>DIRECCION</p>
+                                </li>
+                            </ul>
+                        </div>
+                      <div class="col-lg-6">
+                            <ul class="list-unstyled">
+                            <button type="button" class="btn btn-success">Editar</button>
+                            </ul>
+                     </div>
+
+                    </div>
+                    <!-- /.row -->
+                </div>
+
+                <div class="well">
+                    <h4>CARGAS ADICIONALES</h4>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <ul class="list-unstyled">
+                                <li><p>NOMBRE</p>
+                                </li>
+                                <li><p>APELLIDO</p>
+                                </li>
+                                <li><p>EDAD</p>
+                                </li>
+                                <li><p>RUT</p>
+                                </li>
+                                 
+                                </li>
+                            </ul>
+                        </div>
+                     <div class="col-lg-6">
+                            <ul class="list-unstyled">
+                            <button type="button" class="btn btn-success">Editar</button>
+                            </ul>
+                     </div>
+                    </div>
+                    <!-- /.row -->
+                </div>
+
+
+
+
+
+
+
+
+
+			        <!-- Footer -->
+			        <footer>
+			            <div class="row">
+			                <div class="col-lg-12">
+			                    <p>Copyright &copy; Your Website 2014</p>
+			                </div>
+			            </div>
+			        </footer>
+
+			    </div>
+
+
+
+
+
+						<!-- contenido -->
+ </div>
+</div>
+
+        <script src="/js/jquery-3.2.1.min.js"></script>
+        <script src="/js/bootstrap.min.js"></script>
+        <script src="/js/cart.js"></script>
+        <script src="js/jquery.js"></script>
+
+ 
+
+    </body>
+</html>
