b425b71ee30085e562624894a5b28eda6dc8e7c8
Roderick Lagunas <rlagunas@fidelizador.com>
2017-06-06 19:29:42 -0400

    ver/editar de datos personales

--- controller/Backend/ClientController.php
+++ controller/Backend/ClientController.php
@@ -3,13 +3,34 @@ namespace Controller\Backend;
 
 use Library\Controller;
 use Model\Company;
-use Model\User;
+use Model\Entity\User;
 
 
 class ClientController extends Controller {
     static $template = 'Layout/base.html.php';
-    
+
     function indexAction(){
-        return [];
+        $person = $this->getPerson();
+        return ['persona'=>$person];
+    }
+
+    function editAction(){
+        $person = $this->getPerson();
+        return ['persona'=>$person];
+    }
+
+    function saveAction(){
+        $id = (int)$person = $this->getPerson()['id'];
+        $data = $_POST['person'];
+        $user = new User;
+        $user->update($id, $data);
+        $_SESSION['message']="Datos Actualizados con exito";
+        $this->redirect('/backend/client');
+    }
+
+    private function getPerson(){
+        $user = new User;
+        $rut = $_SESSION['user']['rut'];
+        return $user->select('*', "rut='$rut'")->fetch();
     }
 }
--- view/Backend/Client/index.html.php
+++ view/Backend/Client/index.html.php
@@ -1,1 +1,32 @@
-<h1>Pantala cliente</h1>
+<h1>Mis Datos Personales</h1>
+<?php if($_SESSION['message']):?>
+    <p class="alert alert-success"><?=$_SESSION['message']?></p>
+    <?php unset($_SESSION['message']) ?>
+<?php endif ?>
+<table class="table">
+    <tr>
+        <td>Rut</td>
+        <td><?=$persona['rut']?></td>
+    </tr>
+    <tr>
+        <td>Nombre</td>
+        <td><?=$persona['nombre']?></td>
+    </tr>
+    <tr>
+        <td>Apellido</td>
+        <td><?=$persona['apellido']?></td>
+    </tr>
+    <tr>
+        <td>Fecha Nacimiento</td>
+        <td><?=$persona['fecha_nacimiento']?></td>
+    </tr>
+    <tr>
+        <td>Teléfono</td>
+        <td><?=$persona['telefono']?></td>
+    </tr>
+    <tr>
+        <td>Dirección</td>
+        <td><?=$persona['direccion']?></td>
+    </tr>
+</table>
+<a href="/backend/client/edit" class="btn btn-default">Cambiar mis datos</a>
