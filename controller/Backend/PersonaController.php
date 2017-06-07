11942f6e78b25db3c628b1dcaab4380d17634b13
esrosales <estefania.rosales.s@gmail.com>
2017-06-07 17:35:14 -0400

    Mantenedor persona terminado

--- controller/Backend/PersonaController.php
+++ controller/Backend/PersonaController.php
@@ -1,115 +1,47 @@
 <?php
 namespace Controller\Backend;
 
-use Library\Controller;
-use Model\Entity\User;
-use Model\Entity\Ambulancia;
-use Model\Entity\Persona;
-/**
-* 
-*/
-class PersonaController extends Controller
-{
-	
-	static $template = 'Layout/base.html.php';
-    
-    function indexAction(){
-        $user = new User;
-        $users = $user-> getAll();
+use Library\CrudController;
 
-        return ['users' => $users];
+class PersonaController extends CrudController {
+    static $template = 'Layout/base.html.php';
+
+    protected $entity = null;
+    protected $module = 'Persona';
+    protected $route = [
+        'index' => '/backend/Persona',
+        'edit' => '/backend/Persona/edit',
+    ];
+    protected $vtitles = [
+        'index'=>'Listado de Persna',
+        'edit'=>'Editar Persona',
+        'new'=>'Nueva Persona',
+    ];
+    protected $fields = [
+        'rut'=>['name'=>'Rut','type'=>'text'],
+        'password'=>['name'=>'Password','type'=>'text'],
+        'nombre'=>['name'=>'Nombre','type'=>'text'],
+        'apellido'=>['name'=>'Apellido','type'=>'text'],
+        'fecha_nacimiento'=>['name'=>'Fecha Nacimiento','type'=>'text'],
+        'telefono'=>['name'=>'Telefono','type'=>'text'],
+        'direccion'=>['name'=>'Direccion','type'=>'text'],
+        'activo'=>['name'=>'Activo en sistema','type'=>'text'],
+        'gestor'=>['name'=>'Gestor','type'=>'text'],
+        'cliente'=>['name'=>'Cliente','type'=>'text'],
+    ];
+
+    protected $messages = [
+        'save'=>'Persona guardada con exito',
+        'create'=>'Persona creada con exito',
+    ];
+
+    function __construct(){
+        $this->entity = new \Model\Entity\Persona;
     }
 
-    function deleteAction(){
-      $user_id = $_GET['id'];
+     function indexAction(){
 
-      $user = new User;
-      $user->delete($user_id);
-      $this->redirect('/backend/persona?success=true');
+     unset($this->fields['password']);
+      return parent::indexAction();
     }
-
-  function newAction(){
-      return ['title' => "Crear nuevo usuario"];
-    }
-
-
-    function createAction(){
-
-      $query_fields = ['rut'=>$_POST["rut"], 'password'=>$_POST["password"], 'nombre'=>$_POST["nombre"], 'apellido'=>$_POST["apellido"], 'fecha_nacimiento'=>$_POST["fecha_nacimiento"],'direccion'=>$_POST["direccion"],
-        'contacto'=>$_POST["contacto"],'activo'=>$_POST["activo"], 'gestor'=>$_POST["gestor"], 'cliente'=>$_POST["cliente"] ];
-      
-      $user = new User;
-      $user->create($query_fields);
-      $this->redirect('/backend/persona?success=true');
-    }   
-
-
-    function setSelectedClientAction(){
-        // Asigna un cliente 'seleccionado' a una variable de sesion, para que cuando queramos asignar una ambulancia ya sepamos a quien le pertenecera
-
-        $rut = $this->post('rut');
-
-        // ############### MEJORAR SEGURIDAD EVITANDO INJECCION SQL!!! #######
-        $query = "select * from persona where rut = '$rut' and cliente = 1;";
-        $cliente = (new User)->customQuery($query)-> fetch();
-
-        if ($cliente == false) {
-          unset($_SESSION['selectedCliente']);
-          echo json_encode(['success'=> false, 'alert_param'=>'?alert=rut_no_encontrado']); exit;
-        }
-
-        $_SESSION['selectedCliente']= $cliente;
-        echo json_encode(['cliente'=> $cliente, 'alert_param'=>'?alert=cliente_seleccionado', 'success'=> true]); exit;
-        
-    }
-
-    function asignarAmbulanciaAction(){
-      $user_id = $this->post('user_id');
-
-      $query_ambulancias_libres = "select * from ambulancia where persona_id is null limit 1;";
-      $ambulancia = (new Ambulancia)->customQuery($query_ambulancias_libres)->fetch(); 
-      $ambulancia_id = $ambulancia['id'];
-
-      if ($ambulancia == false) {
-        echo json_encode(['success'=> false, 'alert_param'=>'?alert=sin_ambulancias_libres']); exit;
-      }
-
-      $query_usario_con_ambulancia = "select * from ambulancia where persona_id = $user_id limit 1;";
-      $ya_tiene_ambulancia = (new Ambulancia)->customQuery($query_usario_con_ambulancia)->fetch(); 
-      
-      if ($ya_tiene_ambulancia != false) {
-        echo json_encode(['success'=> false, 'alert_param'=>'?alert=ambulancia_ya_asignada']); exit;
-      }     
-      
-      $query_asignar_ambulancia = "update ambulancia set persona_id = $user_id where id = $ambulancia_id;";
-      $ambulancia = (new Ambulancia)->customQuery($query_asignar_ambulancia); 
-
-      echo json_encode(['success'=> true, 'alert_param'=>'?alert=ambulancia_asignada', "ambulancia_id"=> $ambulancia_id]); exit;
-    
-    }
-
-    function editAction(){
-
-        $id = $_GET["id"];
-        $Persona = (new Persona)->getOnePersonaById($id);
-
-        //$user->select(['rut','password','nombre','apellido','fecha_nacimiento', 'direccion', 'contacto', 
-          //               'activo', 'gestor', 'cliente']);
-        //$user ->fetch();
-
-
-        return ['persona'=>$Persona];
-
-    }
-
-    function saveAction(){
-      $id = (int)$_GET["id"];
-      $query_fields = ['rut'=>$_POST["rut"], 'password'=>$_POST["password"], 'nombre'=>$_POST["nombre"], 'apellido'=>$_POST["apellido"], 'fecha_nacimiento'=>$_POST["fecha_nacimiento"],'direccion'=>$_POST["direccion"],
-        'contacto'=>$_POST["contacto"],'activo'=>$_POST["activo"], 'gestor'=>$_POST["gestor"], 'cliente'=>$_POST["cliente"] ];
-      $persona = new Persona;
-      $persona->update($id, $query_fields);
-      $this->redirect('/backend/persona');
-    }
-
-
 }
--- view/Backend/Persona/index.html.php
+++ view/Backend/Persona/index.html.php
@@ -1,32 +1,26 @@
-<a href="/backend/persona/new" class="btn btn-lg btn-info">Crear nueva Persona</a>
-<table class="data-table table table-hover table-striped table-condensed ">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>Rut</th>
-                    <th>Apellido</th>
-                    <th>Direccion</th>
-                    <th>Telefono</th>
-                    <th></th>
-                </tr>
-            </thead>
-
-            <br/><br/><br/>
-            <h3>Usuarios</h3>
-            <br>
-            <tbody>
-                <?php foreach($users as $row): ?>
-                  <tr>
-                      <td><?= $row['id'] ?></td>
-                      <td><?=$row['nombre']?></td>
-                      <td><?= $row["rut"] ?></td>
-                      <td><?= $row["apellido"] ?></td>
-                      <td><?= $row["direccion"] ?></td>
-                      <td><?= $row["contacto"] ?></td>
-                      <td><a href="/backend/persona/delete?id=<?=$row['id']?>" class="btn btn-danger ">Eliminar</a></td>
-                      <td><a href="/backend/persona/edit?id=<?=$row['id']?>" class="btn btn-success">Editar</a></td>
-                  </tr>
-              <?php endforeach ?>
-            </tbody>
-      </table>
+ <h1><?=$vtitle?></h1>
+<a href="/backend/<?=$module?>/new" class="btn btn-success">Nueva <?=ucfirst($module)?></a>
+<table class="table data-table">
+    <thead>
+    <tr>
+        <?php foreach($fields as $field):?>
+        <th><?=$field['name']?></th>
+        <?php endforeach?>
+        <th>Acciones</th>
+    </tr>
+    </thead>
+    <tbody>
+        <?php foreach($entities as $entity):?>
+        <tr>
+            <?php foreach($fields as $key=>$field):?>
+               
+            <td><?=$entity[$key]?></td>
+            <?php endforeach?>
+            <td>
+                <a href="/backend/<?=$module?>/edit?id=<?=$entity['id']?>" class="btn btn-success">Editar</a>
+                <a href="/backend/<?=$module?>/delete?id=<?=$entity['id']?>" class="btn btn-danger">Eliminar</a>
+            </td>
+        </tr>
+    <?php endforeach ?>
+    </tbody>
+</table>
